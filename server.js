edf0a43607e79f8d12f297ee84429678b14dd1d0
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-03-14 22:14:34 +0000

    working through workshop

--- server.js
+++ server.js
@@ -2,12 +2,37 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-    res.send("hello");
+server.get("/", (req, res, next) => {
+    console.log(req.method + " " + req.url);
+    next();
 })
+
+server.get("/", (req, res) => {
+    const time = new Date().toLocaleTimeString();
+    res.send(`<h1>Hello, it's ${time}</h1>`);
+});
+
+server.get("/json", (req, res) => {
+    res.send({message: "Hello"});
+});
+
+server.get("/redirects", (req, res) => {
+    res.redirect("/");
+});
+
+server.get("/users/:name", (req, res) => {
+    const name = req.params.name;
+    res.send(`<h1>Hello ${name}</h1>`);
+});
+
+// Catch-all handler
+
+server.use((req, res) => {
+    res.status(404).send(`<h1>Not found</h1>`);
+});
 
 const PORT = 3000;
 
 server.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
-})
+});
